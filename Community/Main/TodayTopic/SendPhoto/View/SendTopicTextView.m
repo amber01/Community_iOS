9e8d719a65a2d41bc163883a7bc9dc16d2aa663b
amber01 <shlidty@163.com>
2016-04-14 23:11:07 +0800

    bug fix

--- Community/Main/TodayTopic/SendPhoto/View/SendTopicTextView.m
+++ Community/Main/TodayTopic/SendPhoto/View/SendTopicTextView.m
@@ -26,7 +26,7 @@
 //        [CommonClass setBorderWithView:_titleTextField top:NO left:NO bottom:YES right:NO borderColor:LINE_COLOR borderWidth:0.5];
 //        [self addSubview:_titleTextField];
         
-        self.contentTextView = [[UITextView alloc]initWithFrame:CGRectMake(5, 0, frame.size.width - 10, 230)];//34
+        self.contentTextView = [[UITextView alloc]initWithFrame:CGRectMake(5, 0, frame.size.width - 10, (ScreenHeight/2) - 110)];//34
         
         _contentTextView.returnKeyType = UIReturnKeyDefault;
         _contentTextView.keyboardType = UIKeyboardTypeDefault;
